b98b01ec2d4b03adb577b396dcd922ddedb0c037
nezdolik <kateryna.nezdolii@gmail.com>
2016-09-24 18:42:42 +0200

    Rejected commit: Default

---
+++ src/base/Rectangle.java
@@ -0,0 +1,19 @@
+package base;
+
+/**
+ * Created by nezdolik on 24.09.16.
+ */
+public class Rectangle {
+
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public Rectangle(int x1, int y2, int y1, int x2) {
+        this.x1 = x1;
+        this.y2 = y2;
+        this.y1 = y1;
+        this.x2 = x2;
+    }
+}
